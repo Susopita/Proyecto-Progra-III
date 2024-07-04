aae170fb8a81aff3d3f74920b15611236d4454ce
Susopita <thesusot@gmail.com>
2024-07-03 20:55:22 -0500

    Creacion de clase cliente y script instalador
    Hola, aqui lo que hice fue crear la clase clienteSocket para el frontend
    (pytohn) para abstraer y utilizar de manera mas facil la comunicacion
    con el backend.
    Ademas, veran que hay un archivo vimexInstaller.iss que es el script de
    nuetro instalador que encontre, la extension .iss no tiene soporte en
    vsc pero pueden instalar su IDE, se llama "Inne Setup Compiler" y es de
    Inne setup, valga la redundancia. Pueden ir probandolo si quieren,
    principalmente me encargare yo de gestionarlo pero si quieren ayudarme solo
    avisenme para saberlo.
    
    PD: Inne setup compiler es para unicamente windows, si tienen o usan
    otro sistema les recomiendo usar wine para poder ejecutarlo.

--- backend/include/server.hpp
+++ backend/include/server.hpp
@@ -1,3 +1,23 @@
 #include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+const int TIPO = AF_INET;
+const int PROTO = SOCK_STREAM;
+const int PUERTO = 8080;
 
 int server = socket(AF_INET, SOCK_STREAM, 0);
+
+class ServerSocket
+{
+    int server;
+    int clientSocket;
+
+public:
+    ServerSocket();
+    ~ServerSocket();
+    void start();
+    void stop();
+    void send(const char *msg);
+    void receive(char *msg, int size);
+};
